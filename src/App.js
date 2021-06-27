981146573744355c3ca6877411bc5f312c851743
Gary Moore <madmangaz@gmail.com>
2021-06-28 00:44:01 +0100

    Add cards for messages.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
+import { useState } from "react";
 import "./App.css";
-import { testData } from "./util/testData.json";
+import { testMessages } from "./util/testMessages.json";
 
-const NameList = ({ people }) => {
+const Message = ({ username, message }) => {
   return (
-    <div>
-      {people.map((person) => ( 
-        <div>
-          <h1>
-            {person.firstName} {person.lastName}
-          </h1>
-          <p>{person.age}</p>
-        </div>
-      ))}
+    <div className="Card">
+      <div className="MessageHeader">
+        <p>{username}</p>
+      </div>
+      <div>
+        <p className='MessageContent'>{message}</p>
+      </div>
     </div>
   );
 };
 
+const Messages = ({ messages }) => {
+  return messages.map((message) => (
+    <Message username={message.username} message={message.message} />
+  ));
+};
+
+const SendBox = ({ handleSend }) => {
+  return (
+    <div>
+      <input type="text" />
+
+    </div>
+  )
+}
+
 function App() {
+  let messages = useState(...testMessages);
+  const handleSend = (message) => {
+    messages = [...messages, message]
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <NameList people={testData} />
-      </header>
+      <Messages messages={messages} />
+      <SendBox handleSend={handleSend} />
     </div>
   );
 }
